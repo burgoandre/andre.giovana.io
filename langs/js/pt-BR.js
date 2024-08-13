30ebafdd930d451b1d4478316707706d451cad9d
burgoandre <burgo.andre@gmail.com>
2024-08-13 19:33:29 -0300

    Subi os arquivos do meu site

---
+++ langs/js/pt-BR.js
@@ -0,0 +1,38 @@
+var lang_nombreRequerido = 'Insira o nome';
+var lang_informandoAsistencia = 'Relatando presença';
+var lang_confirmarAsistencia = 'Enviar';
+
+var lang_caracteresNombreAsistencia = 'O nome não deve ter mais de 20 caracteres';
+var lang_caracteresComentariosAsistencia = 'A mensagem não deve exceder 100 caracteres';
+
+var lang_asistenciaMsjExito_1 = 'Informação enviada';
+var lang_asistenciaMsjExito_2 = 'Enviamos suas informações de comparecimento';
+
+
+var lang_cancionRequerida = 'Insira o nome da música / autor';
+var lang_linkIncorrecto = 'Formato link incorrecto';
+var lang_enviandoSugerencia = 'Enviando sugestão';
+var lang_sugerirCancion = 'Sugerir música';
+
+
+var lang_caracteresNombreSugerencia = 'O nome não deve ter mais de 20 caracteres';
+var lang_caracteresCancionSugerencia = 'O título / autor não deve exceder 50 caracteres';
+var lang_caracteresLinkSugerencia = 'O link não deve ter mais de 250 caracteres';
+
+var lang_sugerirCancionMsjExito_1 = 'Sugestão enviada';
+var lang_sugerirCancionMsjExito_2 = 'Sua sugestão de música foi adicionada à lista';
+
+
+var lang_titleModalAsistenciaCeremonia = 'Você irá à cerimônia?';
+var lang_titleModalAsistenciaFiesta = 'Você irá à festa?';
+
+var lang_titleModalMapaFiesta = 'Como chegar à festa';
+var lang_titleModalMapaCeremonia = 'Como chegar à Cerimônia';
+
+
+var lang_codigoAccesoVipRequerido = 'Digite o código de acesso';
+var lang_btnCodigoVip = 'Acessar';
+var lang_accediendoCodigoVip = 'Acessando';
+
+
+var lang_textoFinalCuentaRegresiva = 'O GRANDE <br> DIA CHEGOU!';
